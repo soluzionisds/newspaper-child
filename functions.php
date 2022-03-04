25f0e33d30ce5cf4bf0304c1ce0a944f52c9c810
Sebastiano Riva <sebastiano.riva@gmail.com>
2022-03-04 11:40:33 +0100

    added logo functions for login

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@ if ( ! function_exists( 'suffice_child_enqueue_child_styles' ) ) {
 	      'childe2-style', get_stylesheet_directory_uri() . '/style.css', array(), '1.0.11', 'all'
 	    );
 	    wp_enqueue_style( 'childe2-style');
-		
+
 		wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/scripts.js', array( 'jquery' ));
 	 }
 }
@@ -31,7 +31,8 @@ add_action( 'wp_enqueue_scripts', 'Newspaper_child_enqueue_child_styles' );
 previous_post_link( '<span class="previous-post-link">%link</span>', apply_filters( 'wpbf_previous_post_link', __( '&larr; Previous Post', 'page-builder-framework' ) ) );
 next_post_link( '<span class="next-post-link">%link</span>', apply_filters( 'wpbf_next_post_link', __( 'Next Post &rarr;', 'page-builder-framework' ) ) );
 
-/* MemberPress
+/***************************
+* MemberPress
 
 Send transaction "failed" mail also if change status by backoffice
 */
@@ -40,17 +41,43 @@ function mepr_custom_failed_status_email($txn) {
 }
 add_action('mepr-txn-status-failed', 'mepr_custom_failed_status_email');
 
-/* bbPress
+/***************************
+* Login logo
+****************************/
 
-/// remove bbPress breadcrumb
+function lindipendente_login_logo() { ?>
+    <style type="text/css">
+			#login h1 a, .login h1 a {
+			background-image: url(/wp-content/uploads/2021/03/L..png);
+			width: 150px;
+			height: 150px;
+			background-size: 150px 150px;
+			background-repeat: no-repeat;
+			padding-bottom: 10px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'lindipendente_login_logo' );
 
+function lindipendente_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'lindipendente_login_logo_url' );
+
+function lindipendente_login_logo_url_title() {
+    return 'L\'INDIPENDENTE';
+}
+add_filter( 'login_headertext', 'lindipendente_login_logo_url_title' );
+
+/***************************
+* bbPress
+* remove bbPress breadcrumb
 function bm_bbp_no_breadcrumb ($param) {
 	return true;
 }
 add_filter ('bbp_no_breadcrumb', 'bm_bbp_no_breadcrumb');
 
-/// temporary userpage redirection bbPress
-
+* temporary userpage redirection bbPress
 function user_profile_link(){
     $author_id = bbp_get_reply_author_id();
     $user_info = get_userdata($author_id);
