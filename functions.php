5f5986c5541f46274b4e19cadddd8fc78b4128d6
Sebastiano Riva <sebastiano.riva@gmail.com>
2022-04-09 10:55:41 +0200

    css

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles() {
   wp_enqueue_style('td-theme', get_template_directory_uri() . '/style.css', '', TD_THEME_VERSION, 'all' );
-  wp_enqueue_style('td-theme-child', get_stylesheet_directory_uri() . '/style.css', array('td-theme'), '1.0.22', 'all' );
+  wp_enqueue_style('td-theme-child', get_stylesheet_directory_uri() . '/style.css', array('td-theme'), '1.0.23', 'all' );
   wp_enqueue_script('td-custom-script', get_stylesheet_directory_uri() . '/scripts.js', array( 'jquery' ), '1.0.2');
 }
 
@@ -24,7 +24,7 @@ next_post_link( '<span class="next-post-link">%link</span>', apply_filters( 'wpb
 /***************************
 * includes
 ****************************/
-require_once get_stylesheet_directory() . '/api/facebook.php';
+//require_once get_stylesheet_directory() . '/api/facebook.php';
 require_once get_stylesheet_directory() . '/functions/memberpress.php';
 require_once get_stylesheet_directory() . '/functions/login-logo.php';
 
