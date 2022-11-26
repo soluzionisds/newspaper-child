8d92e37d5bc8980cda03a90eea0da4b7eb3cb9cf
Sebastiano Riva <sebastiano.riva@gmail.com>
2022-11-26 11:56:39 +0100

    Coupon page review

--- scripts.js
+++ scripts.js
@@ -1,64 +1,38 @@
-/***********
-*
-* Countdown Timer
-*
-************/
-
-// Set the date we're counting down to
-/// var countDownDate = new Date("Jan 1, 2022 01:00:00").getTime();
-
-// Update the count down every 1 second
-///var x = setInterval(function() {
-
-  // Get today's date and time
-  ///var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  ///var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  /*** var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000); ***/
-
-  // Output the result in an element with id="countdownTimer"
-  ///document.getElementById("countdownTimer").innerHTML = days + " giorni " + hours + " ore " + minutes + " minuti " + seconds + " secondi ";
-
-  // If the count down is over, write some text
-  /*** if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdownTimer").innerHTML = "0 giorni 0 ore 0 minuti 0 secondi";
-  }
-}, 1000); ***/
-
 /*
-Script for gifts, fix some pages
+Script for gifts: flag automatically the gift checkbox
 */
+(function($) {
+  $(document).ready(function() {
+    var input = $('input[name=mpgft-signup-gift-checkbox]');
+    if(input.length) {
+      input.prop('checked', 'true');
+    }
+  });
+})(jQuery);
+
+/* Script for gifts, fix some pages
 window.addEventListener("load", function() {
     var body = document.getElementsByTagName("body")[0];
     var href = window.location.href;
     var coupon = "WOMENSDAY2022";
     if (body.innerHTML.indexOf("Regala un Abbonamento") !== -1) {
         var checkbox = document.getElementById("mpgft-signup-gift-checkbox1");
-		    //NOTA: click triggera gli eventi, l'attributo checked no.
         checkbox.click();
         //checkbox.parentNode.parentNode.className += " mepr-hidden";
         //document.getElementsByName("mepr_coupon_code")[0].value = coupon;
         //document.getElementsByClassName("have-coupon-link")[0].className += " mepr-hidden";
         //document.getElementsByClassName("mepr_price")[0].className += " mepr-hidden";
     }
-    /*if (
+    if (
         href.indexOf("/abbonamento/3-mesi") > -1 ||
         href.indexOf("/abbonamento/6-mesi") > -1 ||
         href.indexOf("/abbonamento/12-mesi") > -1) {
         document.getElementsByName("mepr_coupon_code")[0].value = coupon;
-    }*/
-});
+    }
+});*/
 
-/*
-Script for gifts, deactivate mouse on box to send gift
-*/
+/*Script for gifts, deactivate mouse on box to send gift
+
 window.addEventListener("load", function(){
 	var links = document.getElementsByClassName("mpgft-open-send-gift");
 	var body = document.getElementsByTagName("body")[0];
@@ -77,4 +51,4 @@ window.addEventListener("load", function(){
 			}
 		});
 	}
-});
+});*/
